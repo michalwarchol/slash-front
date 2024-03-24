d8e99f1a5995f098273dbcc8a251dc618c13d377
Michał Warchoł <mwarchol33@gmail.com>
2024-03-24 17:52:27 +0100

    feat(Header): Create header component

---
+++ src/components/Searchbar/Searchbar.tsx
@@ -0,0 +1,54 @@
+"use client";
+import { CloseOutlined, SearchOutlined } from "@ant-design/icons";
+import { Form, Formik } from "formik";
+import { useEffect, useRef, useState } from "react";
+
+import Input from "@/components/Input";
+
+import styles from "./Searchbar.module.scss";
+
+interface IProps {
+  searchPlaceholder: string;
+}
+
+export default function Searchbar({ searchPlaceholder }: IProps) {
+  const [searchOpen, setSearchOpen] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (ref.current) {
+        const elementStyle = window.getComputedStyle(ref.current);
+
+        if (elementStyle.display === "none") {
+          setSearchOpen(true);
+        }
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize();
+  }, []);
+
+  return (
+    <div className={styles.searchbar}>
+      <Formik onSubmit={() => {}} initialValues={{ search: "" }}>
+        {() => (
+          <Form className={styles.form}>
+            {searchOpen && (
+              <Input
+                name="search"
+                className={styles.input}
+                placeholder={searchPlaceholder}
+              />
+            )}
+          </Form>
+        )}
+      </Formik>
+      <div className={styles.iconWrapper} ref={ref}>
+        <div className={styles.icon} onClick={() => setSearchOpen(!searchOpen)}>
+          {searchOpen ? <CloseOutlined /> : <SearchOutlined />}
+        </div>
+      </div>
+    </div>
+  );
+}
