6ca75879f7372ae802c80496aa575dd61e758a9b
Michał Warchoł <mwarchol33@gmail.com>
2024-06-09 00:43:09 +0200

    fix(RemindPassword): Add validation to all fields

--- src/components/RemindPassword/RemindPassword.types.ts
+++ src/components/RemindPassword/RemindPassword.types.ts
@@ -4,3 +4,15 @@ export type TFormValues = {
   newPassword: string;
   confirmNewPassword: string;
 };
+
+export type TErrorParams = {
+  passwordMin: number;
+}
+
+export type TErrorMessages = {
+  email: string;
+  required: string;
+  min: string;
+  password: string;
+  confirmPasswordDoesNotMatch: string;
+}
---
+++ src/components/RemindPassword/RemindPassword.validation.ts
@@ -0,0 +1,24 @@
+import { object, ref, string } from "yup";
+
+import { TErrorMessages } from "./RemindPassword.types";
+
+export default (errorMessages: TErrorMessages, phase: number) =>
+  phase === 1
+    ? object({
+      email: string()
+        .email(errorMessages.email)
+        .required(errorMessages.required),
+    })
+    : object({
+      code: string().required(errorMessages.required),
+      newPassword: string()
+        .min(8, errorMessages.min)
+        .matches(
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$/,
+          errorMessages.password
+        )
+        .required(errorMessages.required),
+      confirmNewPassword: string()
+        .oneOf([ref("password")], errorMessages.confirmPasswordDoesNotMatch)
+        .required(errorMessages.required),
+    });
